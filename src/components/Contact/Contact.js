f8e6a4e798e08683eb38a31faf4548ee8665625f
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-02-04 20:29:43 -0500

    final final

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ export class Contact extends Component {
                 <h1 className="contact__font">Let's Get In Touch!</h1>
                 <p className="contact__font">michaelbeaudry93@gmail.com</p>
                 <p className="contact__font">www.linkedin.com/in/michael-beaudry/</p>
-                <p className="contact__font">6472695512</p>  
+                {/* <p className="contact__font">6472695512</p>   */}
             </div>
         )
     }
