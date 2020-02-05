44262c6bc1aebf85490a5faa4978a6dd782a0d78
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-02-04 21:10:23 -0500

    link added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ export class Contact extends Component {
                 <h1 className="contact__font">Let's Get In Touch!</h1>
                 <p className="contact__font">michaelbeaudry93@gmail.com</p>
                 {/* <p className="contact__font">www.linkedin.com/in/michael-beaudry/</p> */}
-                <a  className="contact__font" href="https://www.linkedin.com/in/michael-beaudry/">http://www.linkedin.com/in/michael-beaudry/</a>
+                <a  className="contact__font contact__link" href="https://www.linkedin.com/in/michael-beaudry/">http://www.linkedin.com/in/michael-beaudry/</a>
                 {/* <p className="contact__font">6472695512</p>   */}
             </div>
             </>
