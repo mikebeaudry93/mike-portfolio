bd0c35e175ecdf48ce2a1ace20edf1e6aaf55b48
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-02-04 20:17:05 -0500

    projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './Projects.scss'
+import {Link} from "react-router-dom"
+import image1 from "../../assets/tictactoe.png"
+import image2 from "../../assets/ufitness.png"
+
 
 export class Projects extends Component {
     render() {
         return (
             <div className="projects-page">
-                <h1 className="p-font">What I've worked on</h1>
+                <h1 className="p-font">Projects I've worked on</h1>
+                <div className="projects-page__box">
+                    <a href="https://youthful-beaver-86231a.netlify.com/"><img className="projects" src={image1} alt="p1"/></a>
+                    <a href="https://happy-mahavira-f477ed.netlify.com/"><img className="projects" src={image2} alt="p2"/></a>
+                </div>
             </div>
         )
     }
