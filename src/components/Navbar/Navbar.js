8a07ea62b9629bec70ebe05679336a4e7290eb3d
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-05 00:36:11 -0400

    lowercase

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,8 +7,8 @@ export class Navbar extends Component {
         return (
             <div className="navbar">
                 <Link to="/" className="links"><p className="navbar__home">HOME</p></Link>
-                <Link to="/Projects" className="links"><p className="navbar__projects"> PROJECTS </p></Link>
-                <Link to="/Contact" className="links"><p className="navbar__contact">CONTACT</p></Link>
+                <Link to="/projects" className="links"><p className="navbar__projects"> PROJECTS </p></Link>
+                <Link to="/contact" className="links"><p className="navbar__contact">CONTACT</p></Link>
             </div>
         )
     }
