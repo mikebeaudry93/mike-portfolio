925d0c896be001757d8b2c51c2f2d32b35fbe2b8
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-04 22:32:11 -0400

    new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import './Projects.scss'
 import {Link} from "react-router-dom"
 import image1 from "../../assets/tictactoe.png"
 import image2 from "../../assets/ufitness.png"
+import image3 from "../../assets/tasklist.png"
 
 
 export class Projects extends Component {
@@ -12,7 +13,8 @@ export class Projects extends Component {
                 <h1 className="p-font">What I've Worked On!</h1>
                 <div className="projects-page__box">
                     <a href="https://youthful-beaver-86231a.netlify.com/"><img className="projects" src={image1} alt="p1"/></a>
-                    <a href="https://happy-mahavira-f477ed.netlify.com/"><img className="projects" src={image2} alt="p2"/></a>
+                    {/* <a href="https://happy-mahavira-f477ed.netlify.com/"><img className="projects" src={image2} alt="p2"/></a> */}
+                    <a href="https://cranky-lamarr-f3677e.netlify.app/"><img className="projects" src={image3} alt="p3"/></a>
                 </div>
             </div>
         )
